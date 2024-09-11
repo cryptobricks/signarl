85fa708556fea1849c7eb04ca9a572f33e077c99
MYMEILE <529266369@qq.com>
2024-09-11 13:31:08 +0800

    update

---
+++ error_message.py
@@ -0,0 +1,50 @@
+import base64
+import hashlib
+import hmac
+from datetime import datetime
+
+import pytz
+import requests
+
+WEBHOOK_URL = "https://open.larksuite.com/open-apis/bot/v2/hook/b4164b01-2d4a-461d-90dc-d54a2f843c24"
+# WEBHOOK_URL = "https://open.larksuite.com/open-apis/bot/v2/hook/a0f1143e-0de1-4d42-9a27-b80bc6fb2833"
+WEBHOOK_SECRET = "pbEQbitncvDQ7npSOpwtDc"
+# WEBHOOK_SECRET = "OJK9W6jYxnxoLS9Dlm3VOh"
+
+
+def gen_sign(secret):  # 拼接时间戳以及签名校验
+    timestamp = int(datetime.now().timestamp())
+    string_to_sign = '{}\n{}'.format(timestamp, secret)
+
+    # 使用 HMAC-SHA256 进行加密
+    hmac_code = hmac.new(
+        string_to_sign.encode("utf-8"), digestmod=hashlib.sha256
+    ).digest()
+
+    # 对结果进行 base64 编码
+    sign = base64.b64encode(hmac_code).decode('utf-8')
+
+    return sign
+
+
+def send_error_a_message(content):
+    sign = gen_sign(WEBHOOK_SECRET)
+    shanghai_timezone = pytz.timezone('Asia/Shanghai')
+    timestamp = int(datetime.now(shanghai_timezone).timestamp())
+
+    time_str = datetime.now(shanghai_timezone).strftime("%Y-%m-%d %H:%M:%S")  # 格式化时间字符串
+
+    params = {
+        "timestamp": timestamp,
+        "sign": sign,
+        "msg_type": "text",
+        "content": {"text": f"{time_str}\n{content}"},
+    }
+
+    resp = requests.post(WEBHOOK_URL, json=params)
+    resp.raise_for_status()
+    result = resp.json()
+    if result.get("code") and result.get("code") != 0:
+        print(f"发送失败：{result['msg']}")
+        return
+    print("消息发送成功")
--- signarl.py
+++ signarl.py
@@ -16,6 +16,7 @@ import loguru
 import requests
 
 from demo import t_signal_table
+from error_message import send_error_a_message
 from message import send_a_message
 
 signarl_list = list()
@@ -29,6 +30,46 @@ engine = create_async_engine(
 async_session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
 )
+
+
+async def signal_entry_table(extracted_records):
+    try:
+        utc_time = datetime.utcnow().replace(tzinfo=pytz.utc)
+        formatted_time = utc_time.strftime('%Y-%m-%d %H:%M:%S')
+        timestamp = int(utc_time.timestamp() * 1000)
+
+        if extracted_records:
+            for item in extracted_records:
+                information = {
+                    "signal_id": item.get("id"),
+                    "modelId": item.get("modelId"),
+                    "generateTime": item.get("generateTime"),
+                    "scoreType": item.get("scoreType"),
+                    "text": item.get("text"),
+                    "detail": item.get("detail"),
+                    "checked": item.get("checked"),
+                    "scoreDetail": item.get("scoreDetail"),
+                    "longChecked": item.get("longChecked"),
+                    "checkedTrans": item.get("checkedTrans"),
+                    "symbol": item.get("symbol"),
+                    "model_name": item.get("model_name"),
+                    "model_type_name": item.get("model_type_name"),
+                    "process_rate": item.get("process_rate"),
+                    "create_time": timestamp,
+                    "update_time": timestamp,
+                    "create_at": formatted_time,
+                }
+                async with async_session() as db_session:
+                    async with db_session.begin():
+                        query = t_signal_table.insert().values(information)
+                        await db_session.execute(query)
+                        await db_session.commit()
+                        await db_session.close()
+
+    except Exception as e:
+        loguru.logger.info(e)
+        loguru.logger.error(traceback.format_exc())
+        raise e
 
 
 class GetSignarl(BaseJob):
@@ -51,10 +92,6 @@ class GetSignarl(BaseJob):
 
     async def get_signarl_list(self):
         try:
-            utc_time = datetime.utcnow().replace(tzinfo=pytz.utc)
-            formatted_time = utc_time.strftime('%Y-%m-%d %H:%M:%S')
-            timestamp = int(utc_time.timestamp() * 1000)
-
             url = "https://signarl.com/api/model/signal/list"
             async with aiohttp.ClientSession() as session:
                 async with session.get(url, ssl=False) as response:
@@ -62,102 +99,99 @@ class GetSignarl(BaseJob):
                     result_data = response_json.get("data")
                     result_list = result_data.get("list")
 
-                    if result_list:
-
-                        async with async_session() as session:
-                            async with session.begin():
-                                query = select(t_signal_table.c.signal_id)
-                                result = await session.execute(query)
-                                await session.commit()
-
-                                signal_ids = result.scalars().all()
-                                await session.close()
-
-                        id_list = [item['id'] for item in result_list]
-                        filtered_result_list = [id for id in id_list if id not in signal_ids]
-
-                        if filtered_result_list:
-                            extracted_records = [record for record in result_list if record['id'] in filtered_result_list]
-                            for item in extracted_records:
-                                detail = json.loads(item['detail'])
-                                if detail:
-                                    open_price = detail['checked'].get('close')
-                                else:
-                                    open_price = ""
-
-                                body = {
-                                    "data": [
-                                        {
-                                            "symbol": item['symbol'],
-                                            "model_name": item['model_name'],
-                                            "model_type_name": item["model_type_name"],
-                                            "create_time": item['generateTime'],
-                                            "open_price": open_price,
-                                            "deep_low": item['scoreDetail'].get("min_depth"),
-                                            "deep_high": item['scoreDetail'].get("max_depth"),
-                                            "period_low": item['scoreDetail'].get("min_time"),
-                                            "period_high": item['scoreDetail'].get("max_time")
-                                        }
-                                    ]
-                                }
-
-                                body_json = json.dumps(body)
-                                body_requests = body_json+str(timestamp)+'n9jl2OU8SvHzRKrbGa10xtCM3Qc6V7gA'
-
-                                input_bytes = body_requests.encode('utf-8')
-                                md5 = hashlib.md5()
-                                md5.update(input_bytes)
-                                md5_digest = md5.hexdigest()
-
-                                header = {
-                                    "push-timestamp": str(timestamp),
-                                    "push-sign": md5_digest
-                                }
-
-                                url = 'http://test2.admin.martingrid.com/sapiv2/pushAiSignal'
-                                async with aiohttp.ClientSession() as session:
-                                    async with session.post(url, headers=header, data=body_json, ssl=False) as response:
-                                        response_json = await response.text()
-                                        response_json = json.loads(response_json)
-                                        if response_json == 200:
-                                            loguru.logger.info(response_json)
-                                        else:
-                                            loguru.logger.error(response_json)
-
-                                information = {
-                                    "signal_id": item.get("id"),
-                                    "modelId": item.get("modelId"),
-                                    "generateTime": item.get("generateTime"),
-                                    "scoreType": item.get("scoreType"),
-                                    "text": item.get("text"),
-                                    "detail": item.get("detail"),
-                                    "checked": item.get("checked"),
-                                    "scoreDetail": item.get("scoreDetail"),
-                                    "longChecked": item.get("longChecked"),
-                                    "checkedTrans": item.get("checkedTrans"),
-                                    "symbol": item.get("symbol"),
-                                    "model_name": item.get("model_name"),
-                                    "model_type_name": item.get("model_type_name"),
-                                    "process_rate": item.get("process_rate"),
-                                    "create_time": timestamp,
-                                    "update_time": timestamp,
-                                    "create_at": formatted_time,
-                                }
-                                async with async_session() as db_session:
-                                    async with db_session.begin():
-                                        query = t_signal_table.insert().values(information)
-                                        await db_session.execute(query)
-                                        await db_session.commit()
-                                        await db_session.close()
-
             return result_list
         except Exception as e:
             loguru.logger.exception(traceback.format_exc())
+            raise e
+
+    async def verify_old_signal(self, result_list):
+        try:
+            if result_list:
+                async with async_session() as session:
+                    async with session.begin():
+                        query = select(t_signal_table.c.signal_id)
+                        result = await session.execute(query)
+                        await session.commit()
+
+                        signal_ids = result.scalars().all()
+                        await session.close()
+
+                id_list = [item['id'] for item in result_list]
+                filtered_result_list = [id for id in id_list if id not in signal_ids]
+                if filtered_result_list:
+                    extracted_records = [record for record in result_list if record['id'] in filtered_result_list]
+                    return extracted_records
+        except Exception as e:
+            loguru.logger.exception(e)
+            loguru.logger.error(traceback.format_exc())
+            raise e
+
+    async def sending_a_signal(self, extracted_records):
+        try:
+            utc_time = datetime.utcnow().replace(tzinfo=pytz.utc)
+            formatted_time = utc_time.strftime('%Y-%m-%d %H:%M:%S')
+            timestamp = int(utc_time.timestamp() * 1000)
+
+            if extracted_records:
+                for item in extracted_records:
+                    detail = json.loads(item['detail'])
+                    if detail:
+                        open_price = detail['checked'].get('close')
+                    else:
+                        open_price = ""
+
+                    body = {
+                        "data": [
+                            {
+                                "symbol": item['symbol'],
+                                "model_name": item['model_name'],
+                                "model_type_name": item["model_type_name"],
+                                "create_time": item['generateTime'],
+                                "open_price": open_price,
+                                "deep_low": item['scoreDetail'].get("min_depth"),
+                                "deep_high": item['scoreDetail'].get("max_depth"),
+                                "period_low": item['scoreDetail'].get("min_time"),
+                                "period_high": item['scoreDetail'].get("max_time")
+                            }
+                        ]
+                    }
+
+                    body_json = json.dumps(body)
+                    body_requests = body_json + str(timestamp) + 'n9jl2OU8SvHzRKrbGa10xtCM3Qc6V7gA'
+
+                    input_bytes = body_requests.encode('utf-8')
+                    md5 = hashlib.md5()
+                    md5.update(input_bytes)
+                    md5_digest = md5.hexdigest()
+
+                    header = {
+                        "push-timestamp": str(timestamp),
+                        "push-sign": md5_digest
+                    }
+
+                    url = 'http://test2.admin.martingrid.com/sapiv2/pushAiSignal'
+                    async with aiohttp.ClientSession() as session:
+                        async with session.post(url, headers=header, data=body_json, ssl=False) as response:
+                            response_json = await response.text()
+                            response_json = json.loads(response_json)
+                            if response_json == 200:
+                                loguru.logger.info(response_json)
+                            else:
+                                loguru.logger.error(response_json)
+        except Exception as e:
+            loguru.logger.info(e)
+            loguru.logger.error(traceback.format_exc())
 
     async def get_signarl(self):
         try:
 
             result_list = await self.get_signarl_list()
+
+            old_signal = await self.verify_old_signal(result_list)
+            await signal_entry_table(old_signal)
+
+            if old_signal:
+                await self.sending_a_signal(old_signal)
 
             for index, signarl in enumerate(result_list):
                 generateTime = signarl.get("generateTime")
@@ -179,13 +213,14 @@ class GetSignarl(BaseJob):
 
                 if (model_type_name == "强空" or model_type_name == "强多" or
                         model_type_name == "超空" or model_type_name == "超多" or
-                    model_type_name == "中空" or model_type_name == "中多"
+                        model_type_name == "中空" or model_type_name == "中多"
                 ):
                     signarl_list.append(generateTime)
                     send_a_message(text)
 
         except Exception as e:
             loguru.logger.exception(traceback.format_exc())
+            send_error_a_message(e)
 
     async def do_job(self):
         loguru.logger.info("---->{signarl} start")
